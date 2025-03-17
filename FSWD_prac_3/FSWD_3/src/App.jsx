351b83f9db033cc8474089b02957946b26d1ae78
jameskalvium <jamesrjacob9747@gmail.com>
2025-03-17 18:03:30 +0530

    Add files via upload

---
+++ FSWD_prac_3/FSWD_3/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import {Routes, Route} from "react-router-dom"
+import studentlist from "./componenets/studentList"
+import studentform from "./componenets/studentForm"
+import './App.css'
+
+
+function App() {
+  
+
+  return (
+    
+  )
+}
+
+export default App
